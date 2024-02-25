db4f22f4c73432dfc5c393692d37929bf872896a
Kareem El Hosany <karimcombra189@gmail.com>
2024-02-26 00:44:11 +0200

    login page

--- Login_Simple_3/login_page.dart
+++ Login_Simple_3/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:one_app/login_page.dart';
+import 'package:one_app/sign_up_screan.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,175 +10,166 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  late Color myColor;
-  late Size mediaSize;
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  bool rememberUser = false;
+  late TextEditingController emailController, passwordController;
+
+  @override
+  void initState() {
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    myColor = Theme.of(context).primaryColor;
-    mediaSize = MediaQuery.of(context).size;
-    return Container(
-      decoration: BoxDecoration(
-        color: myColor,
-        image: DecorationImage(
-          image: const AssetImage("assets/images/bg.png"),
-          fit: BoxFit.cover,
-          colorFilter:
-              ColorFilter.mode(myColor.withOpacity(0.2), BlendMode.dstATop),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset('Assets/images/app_logo.jpg', fit: BoxFit.cover),
       ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Stack(children: [
-          Positioned(top: 80, child: _buildTop()),
-          Positioned(bottom: 0, child: _buildBottom()),
-        ]),
-      ),
-    );
-  }
-
-  Widget _buildTop() {
-    return SizedBox(
-      width: mediaSize.width,
-      child: const Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(
-            Icons.location_on_sharp,
-            size: 100,
-            color: Colors.white,
-          ),
-          Text(
-            "GO MAP",
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 40,
-                letterSpacing: 2),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _buildBottom() {
-    return SizedBox(
-      width: mediaSize.width,
-      child: Card(
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(30),
-          topRight: Radius.circular(30),
-        )),
-        child: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: _buildForm(),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildForm() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Welcome",
-          style: TextStyle(
-              color: myColor, fontSize: 32, fontWeight: FontWeight.w500),
-        ),
-        _buildGreyText("Please login with your information"),
-        const SizedBox(height: 60),
-        _buildGreyText("Email address"),
-        _buildInputField(emailController),
-        const SizedBox(height: 40),
-        _buildGreyText("Password"),
-        _buildInputField(passwordController, isPassword: true),
-        const SizedBox(height: 20),
-        _buildRememberForgot(),
-        const SizedBox(height: 20),
-        _buildLoginButton(),
-        const SizedBox(height: 20),
-        _buildOtherLogin(),
-      ],
-    );
-  }
-
-  Widget _buildGreyText(String text) {
-    return Text(
-      text,
-      style: const TextStyle(color: Colors.grey),
-    );
-  }
-
-  Widget _buildInputField(TextEditingController controller,
-      {isPassword = false}) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-        suffixIcon: isPassword ? Icon(Icons.remove_red_eye) : Icon(Icons.done),
-      ),
-      obscureText: isPassword,
-    );
-  }
-
-  Widget _buildRememberForgot() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Colors.blue,
+          Colors.blueAccent.shade100,
+        ])),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Checkbox(
-                value: rememberUser,
-                onChanged: (value) {
-                  setState(() {
-                    rememberUser = value!;
-                  });
-                }),
-            _buildGreyText("Remember me"),
+            const SizedBox(height: 5),
+            _buildHeader(),
+            const SizedBox(height: 5),
+            Expanded(child: _buildInputFields())
           ],
         ),
-        TextButton(
-            onPressed: () {}, child: _buildGreyText("I forgot my password"))
-      ],
-    );
-  }
-
-  Widget _buildLoginButton() {
-    return ElevatedButton(
-      onPressed: () {
-        debugPrint("Email : ${emailController.text}");
-        debugPrint("Password : ${passwordController.text}");
-      },
-      style: ElevatedButton.styleFrom(
-        shape: const StadiumBorder(),
-        elevation: 20,
-        shadowColor: myColor,
-        minimumSize: const Size.fromHeight(60),
       ),
-      child: const Text("LOGIN"),
     );
   }
 
-  Widget _buildOtherLogin() {
-    return Center(
+  Widget _buildHeader() {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          _buildGreyText("Or Login with"),
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Tab(icon: Image.asset("assets/images/facebook.png")),
-              Tab(icon: Image.asset("assets/images/twitter.png")),
-              Tab(icon: Image.asset("assets/images/github.png")),
-            ],
-          )
+          Text(
+            "Welcome Back!",
+            style: TextStyle(color: Colors.white, fontSize: 40),
+          ),
+          Text(
+            "Login to continue",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2),
+          ),
         ],
       ),
     );
   }
+
+  Widget _buildInputFields() {
+    return Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          color: Colors.white60,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(60),
+            topRight: Radius.circular(60),
+          )),
+      padding: const EdgeInsets.all(20),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 32),
+            Column(
+              children: [
+                _buildTextField("Enter email here", emailController),
+                const SizedBox(height: 16),
+                _buildTextField("Password", passwordController, isPass: true),
+              ],
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "Forgot Password",
+                  style: TextStyle(color: Colors.lightBlueAccent),
+                )),
+            const SizedBox(height: 5),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SignUpPage()));
+                },
+                child: const Text(
+                  "Don't have an account ? Sign Up",
+                  style: TextStyle(color: Colors.lightBlueAccent),
+                )),
+            const SizedBox(height: 10),
+            _buildButton("Login", Colors.lightBlueAccent, () {
+              debugPrint("Email : ${emailController.text}");
+              debugPrint("Password : ${passwordController.text}");
+            }),
+            const SizedBox(height: 10),
+            const Text(
+              "Or Login with",
+              style: TextStyle(color: Colors.lightBlueAccent),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(child: _buildButton("Facebook", Colors.blue, () {})),
+                const SizedBox(width: 32),
+                Expanded(child: _buildButton("Google", Colors.red, () {})),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String hint, TextEditingController controller,
+      {isPass = false}) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          border: Border(bottom: BorderSide(color: Colors.grey))),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+            hintText: hint,
+            hintStyle: const TextStyle(color: Colors.grey),
+            border: InputBorder.none),
+        style: const TextStyle(color: Colors.black),
+        obscureText: isPass,
+      ),
+    );
+  }
+
+  Widget _buildButton(String buttonText, Color color, Function()? onPressed) {
+    return ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          elevation: 2,
+          backgroundColor: color,
+          minimumSize: const Size.fromHeight(60),
+        ),
+        child: Text(buttonText,
+            style: const TextStyle(fontSize: 16, color: Colors.white)));
+  }
 }
+
